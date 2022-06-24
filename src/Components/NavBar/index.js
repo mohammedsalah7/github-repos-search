327434c32678b1014bf895ba0d8a31b30c825638
mohammedsalah7 <mohmsal96@gmail.com>
2022-06-24 17:53:42 +0300

    make style to navbar component

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import {  Link } from 'react-router-dom';
+import "./style.css";
 
 const NavBar = () => {
-    return (
-        <div>
-            <Link  to="/">Home</Link>
-            <Link  to="/comparison">Comparison</Link>
-        </div>
+    return (        
+         <header>
+         <h1 id="nav-title">GitHub</h1>
+         <nav>
+             <ul>
+             <Link  to="/" className='link'>Home</Link>
+            <Link  to="/comparison"  className='link'>Comparison</Link>
+             </ul>
+         </nav>
+     </header>
     );
 }
 
